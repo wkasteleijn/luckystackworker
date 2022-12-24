1a1e123511b9d99b72da9ebd4b3173bd6a8bef35
Wilco Kasteleijn <w.kasteleijn@gmail.com>
2022-12-24 21:50:05 +0100

    savgolay WIP

--- backend/src/main/java/nl/wilcokas/luckystackworker/filter/SavitzkyGolayFilter.java
+++ backend/src/main/java/nl/wilcokas/luckystackworker/filter/SavitzkyGolayFilter.java
@@ -49,8 +49,11 @@ public class SavitzkyGolayFilter {
 
     private static final int RADIUS_25_CENTER = 12;
     private static final int RADIUS_25_DIVISOR = 2447;
-    private static final int RADIUS_49_CENTER = 27;
+    private static final int RADIUS_25_ROWLENGTH = 5;
+
+    private static final int RADIUS_49_CENTER = 24;
     private static final int RADIUS_49_DIVISOR = 4287;
+    private static final int RADIUS_49_ROWLENGTH = 7;
 
     private static final int SHORT_HALF_SIZE = 32768;
     private static final int MAX_INT_VALUE = 65535;
@@ -61,24 +64,28 @@ public class SavitzkyGolayFilter {
         int[][] radiusOffsets;
         int radiusDivisor;
         int radiusCenter;
+        int radiusRowLength;
         switch (radius) {
         case RADIUS_25 -> {
             radiusFactors = RADIUS_25_FACTORS;
             radiusOffsets = RADIUS_25_OFFSETS;
             radiusDivisor = RADIUS_25_DIVISOR;
             radiusCenter = RADIUS_25_CENTER;
+            radiusRowLength = RADIUS_25_ROWLENGTH;
         }
         case RADIUS_49 -> {
             radiusFactors = RADIUS_49_FACTORS;
             radiusOffsets = RADIUS_49_OFFSETS;
             radiusDivisor = RADIUS_49_DIVISOR;
             radiusCenter = RADIUS_49_CENTER;
+            radiusRowLength = RADIUS_49_ROWLENGTH;
         }
         default -> {
             radiusFactors = RADIUS_25_FACTORS;
             radiusOffsets = RADIUS_25_OFFSETS;
             radiusDivisor = RADIUS_25_DIVISOR;
             radiusCenter = RADIUS_25_CENTER;
+            radiusRowLength = RADIUS_25_ROWLENGTH;
         }
         }
 
@@ -87,8 +94,8 @@ public class SavitzkyGolayFilter {
             short[] pixels = (short[]) p.getPixels();
             short[] pixelsResult = pixels.clone();
             for (int i = 0; i < pixels.length; i++) {
-                int newValueUnsignedInt = getPixelValueUnsignedInt(pixels, i, image.getWidth(), image.getHeight(), radiusFactors, radiusOffsets,
-                        radiusDivisor, radiusCenter);
+                int newValueUnsignedInt = getPixelValueUnsignedInt(pixels, i, image.getWidth(), radiusFactors, radiusOffsets,
+                        radiusDivisor, radiusCenter, radiusRowLength);
                 pixelsResult[i] = convertToShort(
                         newValueUnsignedInt > MAX_INT_VALUE ? MAX_INT_VALUE : (newValueUnsignedInt < 0 ? 0 : newValueUnsignedInt));
             }
@@ -99,23 +106,23 @@ public class SavitzkyGolayFilter {
         image.updateAndDraw();
     }
 
-    private int getPixelValueUnsignedInt(short[] pixels, final int position, final int width, final int height, int[] radiusFactors,
-            int[][] radiusOffsets, int radiusDivisor, int radiusCenter) {
+    private int getPixelValueUnsignedInt(short[] pixels, final int position, final int width, int[] radiusFactors,
+            int[][] radiusOffsets, int radiusDivisor, int radiusCenter, int radiusRowLength) {
         final int yPosition = position / width;
-        final int xPosition = position % height;
+        final int xPosition = position % width;
         List<Integer> multipliedValues = new ArrayList<>();
         for (int i = 0; i < radiusOffsets.length; i++) {
             int xOffset = radiusOffsets[i][0];
             int yOffset = radiusOffsets[i][1];
             int offsetValueUnsignedInt = getOffsetValueUnsignedInt(pixels, xPosition + xOffset, yPosition + yOffset, width);
-            int factor = radiusFactors[radiusCenter + (yOffset * 5) + xOffset];
+            int factor = radiusFactors[radiusCenter + (yOffset * radiusRowLength) + xOffset];
             multipliedValues.add(offsetValueUnsignedInt * factor);
         }
         return multipliedValues.stream().reduce(Integer::sum).orElse(0) / radiusDivisor;
     }
 
     private int getOffsetValueUnsignedInt(short[] pixels, final int xPosition, final int yPosition, final int width) {
-        int position = yPosition * width + xPosition;
+        int position = (yPosition * width) + xPosition;
         if (position < 0 || position >= pixels.length) {
             return 0;
         }
