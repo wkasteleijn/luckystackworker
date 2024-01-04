dabde3bd6d504bc97e88ddc010d4de359596a84b
Wilco <w.kasteleijn@heartforhealth.com>
2024-01-04 19:57:55 +0100

    Working gmic controls on macos

--- backend/src/main/java/nl/wilcokas/luckystackworker/service/GmicService.java
+++ backend/src/main/java/nl/wilcokas/luckystackworker/service/GmicService.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import ij.ImageStack;
 import ij.io.Opener;
 import ij.process.ImageProcessor;
 import lombok.extern.slf4j.Slf4j;
+import nl.wilcokas.luckystackworker.constants.Constants;
 import nl.wilcokas.luckystackworker.exceptions.FilterException;
 import nl.wilcokas.luckystackworker.util.Util;
 
@@ -21,14 +24,16 @@ import nl.wilcokas.luckystackworker.util.Util;
 @Service
 public class GmicService {
     public void callGmicCli(ImagePlus image, final String profileName, final List<String> commands) throws IOException, InterruptedException {
-        String workFolder = Util.getIJFileFormat(System.getProperty("user.home")) + "/AppData/Local/LuckyStackWorker";
+    	String activeOSProfile = Util.getActiveOSProfile();
+    	String workFolder = Util.getDataFolder(activeOSProfile);
         String inputFile = workFolder + "/temp_in.tif";
         Util.saveImage(image, profileName, inputFile, true, false, false, false);
         String outputFile = workFolder + "/temp_out.tif";
-        List<String> arguments = new ArrayList<>(Arrays.asList("./gmic/gmic.exe", "v", "2", "-input", inputFile, "-div", "65536"));
+        List<String> arguments = new ArrayList<>(Arrays.asList("v", "2", "-input", inputFile, "-div", "65536"));
+        arguments.addAll(0, getGmicCommand(activeOSProfile));
         arguments.addAll(commands);
         arguments.addAll(Arrays.asList("-mul", "65536", "-output", outputFile + ",int16"));
-        ProcessBuilder processBuilder = new ProcessBuilder(arguments);
+        ProcessBuilder processBuilder = getProcessBuilder(activeOSProfile, arguments);
         processBuilder.redirectErrorStream(true);
         if (logOutput(processBuilder.start()) != 0) {
             throw new FilterException("G'mic CLI execution failed");
@@ -51,5 +56,22 @@ public class GmicService {
         log.info(IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8.name()));
         log.info("==== G'mic CLI output end ====");
         return process.waitFor();
+    }
+    
+    private List<String> getGmicCommand(String activeOSProfile) {
+    	if (Constants.SYSTEM_PROFILE_WINDOWS.equals(activeOSProfile)) {
+    		return Collections.singletonList("./gmic/gmic.exe");
+    	} else {
+    		return Arrays.asList("export", "PATH=/usr/local/bin:/opt/homebrew:$PATH", "&&", "gmic");
+    	}
+    }
+    
+    private ProcessBuilder getProcessBuilder(String activeOSProfile, List<String> arguments) {
+    	if (Constants.SYSTEM_PROFILE_WINDOWS.equals(activeOSProfile)) {
+    		return new ProcessBuilder(arguments);
+    	} else {
+        	String joinedArguments = arguments.stream().collect(Collectors.joining(" "));
+            return new ProcessBuilder("zsh","-c", joinedArguments);
+    	} 
     }
 }
