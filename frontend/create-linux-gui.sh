c2d5ae364facd9733e6f64618c54ecb4f689e5d2
Wilco Kasteleijn <w.kasteleijn@gmail.com>
2024-01-21 17:42:00 +0100

    Updated linux & mac create scripts

--- frontend/create-linux-gui.sh
+++ frontend/create-linux-gui.sh
@@ -3,11 +3,12 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-rm -rf lsw-gui-linux-x64
+rm -rf LuckyStackWorker-linux-x64
 npx electron-packager . lsw-gui --platform linux --arch x64 --icon ../luckystackworker_icon.png --overwrite
-cp ../backend/target/luckystackworker-$1.jar ./lsw-gui-linux-x64
-cp -R ./linux/* ./lsw-gui-linux-x64
-cd ./lsw-gui-linux-x64/resources/app
+mv lsw-gui-linux-x64 LuckyStackWorker-linux-x64
+cp ../backend/target/luckystackworker-$1.jar ./LuckyStackWorker-linux-x64
+cp -R ./linux/* ./LuckyStackWorker-linux-x64
+cd ./LuckyStackWorker-linux-x64/resources/app
 rm -rf node_modules
 rm -rf src
 rm -rf .angular
@@ -16,4 +17,5 @@ rm -rf reset_db.sql
 rm -rf jre
 rm -rf launch4j_lsw.xml
 rm -rf LuckyStackWorker-win32-x64
+rm -rf linux
 rm -f LuckyStackWorker.exe
