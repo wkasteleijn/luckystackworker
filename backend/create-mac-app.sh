49de268d85de9019d3bed56e6bb2e79f6021feee
Wilco Kasteleijn <w.kasteleijn@gmail.com>
2024-04-30 09:10:58 +0200

    Removed version from packaging scripts, no longer needed, removed old db file, no longer used

--- backend/create-mac-app.sh
+++ backend/create-mac-app.sh
@@ -1,24 +1,24 @@
 if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 [lsw_version] [architecture] (x64 or arm64)"
+  echo "Usage: $0 [architecture] (x64 or arm64)"
   exit 1
 fi
 
-if [ "$2" = "x64" ]; then
-    echo "Building the LSW for $2 architecture"
+if [ "$1" = "x64" ]; then
+    echo "Building the LSW for $1 architecture"
     export JRE_HOME="/Users/wkasteleijn/Applications/jre21-zulu-x64/Java"
 fi
 
-if [ "$2" = "arm64" ]; then
-    echo "Building the LSW for $2 architecture"
+if [ "$1" = "arm64" ]; then
+    echo "Building the LSW for $1 architecture"
     export JRE_HOME="/Users/wkasteleijn/Applications/jre21-zulu-arm64/Java"
 fi
 
 cd ~/git/luckystackworker/backend/target
 rm -rf LuckyStackWorker.app
 jar2app -r $JRE_HOME -i ../../frontend/luckystackworker_icon.icns -n "LuckyStackWorker" \
-    --jvm-options="-Dspring.profiles.active=mac -Dlsw.version=$1" luckystackworker-$1.jar
+    --jvm-options="-Dspring.profiles.active=mac" luckystackworker.jar
 cd LuckyStackWorker.app/Contents/MacOS
 rm -rf JavaAppLauncher
 cp ~/Applications/universalJavaApplicationStub-custom-src JavaAppLauncher
-cp -Ra ~/git/luckystackworker/frontend/lsw-gui-darwin-$2/lsw-gui.app .
+cp -Ra ~/git/luckystackworker/frontend/lsw-gui-darwin-$1/lsw-gui.app .
 cp -f ~/.lsw/lsw_db.mv.db ../Resources
--- frontend/create-linux-gui.sh
+++ frontend/create-linux-gui.sh
@@ -1,12 +1,7 @@
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 version"
-  exit 1
-fi
-
 rm -rf LuckyStackWorker-linux-x64
 npx electron-packager . lsw-gui --platform linux --arch x64 --icon ../luckystackworker_icon.png --overwrite
 mv lsw-gui-linux-x64 LuckyStackWorker-linux-x64
-cp ../backend/target/luckystackworker-$1.jar ./LuckyStackWorker-linux-x64
+cp ../backend/target/luckystackworker.jar ./LuckyStackWorker-linux-x64
 cd ./LuckyStackWorker-linux-x64/resources/app
 rm -rf node_modules
 rm -rf src
