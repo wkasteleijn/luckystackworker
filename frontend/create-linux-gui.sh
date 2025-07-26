cce1d1e632381309e85791bf8db7bca9babe271a
Wilco Kasteleijn <w.kasteleijn@gmail.com>
2025-07-26 14:18:55 +0200

    Small change to the linux packaging script

--- frontend/create-linux-gui.sh
+++ frontend/create-linux-gui.sh
@@ -1,6 +1,6 @@
 rm -rf LuckyStackWorker-linux-x64
 npx electron-packager . LuckyStackWorker --platform linux --arch x64 --icon ../graphics/luckystackworker_icon.png --overwrite
-cp ../backend/target/luckystackworker.jar ./LuckyStackWorker-linux-x64
+cp ../backend/build/libs/luckystackworker.jar ./LuckyStackWorker-linux-x64
 cd ./LuckyStackWorker-linux-x64/resources/app
 rm -rf node_modules
 rm -rf src
